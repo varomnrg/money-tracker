cb8bc55b86b3410d6e6482ab52fdeed0f5cf010b
Timothy Aurelio Cannavaro <89766992+varomnrg@users.noreply.github.com>
2023-12-10 08:33:33 +0000

    feat(repository): add category repository

--- repository/category/category_postgresql.go
+++ repository/category/category_postgresql.go
@@ -6,6 +6,7 @@ import (
 	"log"
 
 	_ "github.com/lib/pq"
+	"github.com/varomnrg/money-tracker/model"
 )
 
 type postgresqlCategoryRepository struct {
@@ -32,4 +33,103 @@ func NewPostgresqlCategoryRepository(DB_URL string) *postgresqlCategoryRepositor
 	return &postgresqlCategoryRepository{
 		connectionPool: connectionPool,
 	}
+}
+
+func (p *postgresqlCategoryRepository) GetCategories() ([]model.Category, error){
+	rows, err := p.connectionPool.Query("SELECT id, name, user_id FROM categories")
+	
+	if err  != nil {
+		return []model.Category{}, err
+	}
+	
+	defer rows.Close()
+
+	categories := make([]model.Category, 0)
+
+	for rows.Next(){
+		category := model.Category{}
+		err := rows.Scan(&category.ID, &category.Name, &category.User_ID)
+		if err != nil {
+			return categories, err
+		}
+		categories = append(categories, category)
+	}
+	
+	return categories, nil
+}
+
+func (p *postgresqlCategoryRepository) GetUserCategories(userID string) ([]model.Category, error){
+	rows, err := p.connectionPool.Query("SELECT id, name, user_id FROM categories WHERE user_id = $1", userID)
+
+	if err != nil {
+		return []model.Category{}, err
+	}
+
+	defer rows.Close()
+
+	categories := make([]model.Category, 0)
+
+	for rows.Next() {
+		category := model.Category{}
+		err := rows.Scan(&category.ID, &category.Name, &category.User_ID)
+		if err != nil {
+			return categories, err
+		}
+		categories = append(categories, category)
+	}
+
+	return categories, nil
+}
+
+func (p *postgresqlCategoryRepository) GetCategory(id string) (model.Category, error){
+	category := model.Category{}
+
+	row := p.connectionPool.QueryRow("SELECT id, name, user_id FROM categories WHERE id = $1", id)
+
+	err := row.Scan(&category.ID, &category.Name, &category.User_ID)
+
+	if err != nil {
+		return category, err
+	}
+
+	return category, nil
+}
+
+func (p *postgresqlCategoryRepository) CreateCategory(userID string, category model.CategoryRequest) error{
+	_, err := p.connectionPool.Exec(
+		"INSERT INTO categories (name, user_id) VALUES ($1, $2)",
+		category.Name, userID,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *postgresqlCategoryRepository) DeleteCategory(id string) error{
+	_, err := p.connectionPool.Exec(
+		"DELETE FROM categories WHERE id = $1",
+		id,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p *postgresqlCategoryRepository) IsUserCategoryExist(userID string, categoryName string) bool{
+	row := p.connectionPool.QueryRow(
+		"SELECT id FROM categories WHERE user_id = $1 AND name = $2", 
+		userID, categoryName,
+	)
+
+	category := model.Category{}
+
+	err := row.Scan(&category.ID)
+
+	return err == nil
 }
